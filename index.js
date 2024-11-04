58dee8f3cd4e062fcd2a115f7611b1fe78830e36
torofms37 <torofms37@gmail.com>
2024-11-03 22:44:05 -0700

    cambios al js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+//Contenido principal: nombre, info, img y redes
+const principalC = document.getElementById('principal');
+
+//Contenido de portafolio
+const portfolio = document.getElementById('portfolio');
+
+//cuando el usuario toque show button, se muestra el contenido del portafolio y se oculta el principal
+const showButton = document.getElementById('portafolio');
+showButton.addEventListener('click', () => {
+  //se llama a la variable que consigue del documento y se le dice que el estilo a mostrar, será o bien block (mostrar) o none (ocultar)
+  portfolio.style.display = 'block';
+  principalC.style.display = 'none';
+  //en este caso, el portafolio es el contenido que se muestra, por lo que se oculta el principal
+});
+
+const closeButton = document.getElementById('close');
+closeButton.addEventListener('click', () => {
+  //se llama a la variable que consigue del documento y se le dice que el estilo a mostrar, será o bien block (mostrar) o none (ocultar)
+  portfolio.style.display = 'none';
+  principalC.style.display = 'block';
+  //en este caso, el principal es el contenido que se muestra, por lo que se oculta el portafolio
+});
+
 document.getElementById('portafolio').addEventListener('click', function() {
   const container = document.querySelector('.container');
   container.innerHTML = `
       <ul>
-          ${Array.from({length: 4}, (_, i) => `<li>Elemento ${i + 1}</li>`).join('')}
+          ${Array.from({length: 50}, (_, i) => `<li>Elemento ${i + 1}</li>`).join('')}
       </ul>
   `;
-});
-
-
-
-document.getElementById('portafolio').addEventListener('click', function() {
-  document.getElementById('original-content').style.display = 'none';
-  document.getElementById('portafolio').style.display = 'block';
-  document.getElementById('back-btn').style.display = 'block';
-});
-
-document.getElementById('back-btn').addEventListener('click', function() {
-  document.getElementById('original-content').style.display = 'block';
-  document.getElementById('portafolio').style.display = 'none';
-  document.getElementById('back-btn').style.display = 'none';
 });
