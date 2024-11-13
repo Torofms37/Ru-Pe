0d199e345c1353d899805b65909db9ff7f564818
torofms37 <torofms37@gmail.com>
2024-11-13 00:35:59 -0700

    cambios a portfolio-secyion y se agrego cards

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-//Contenido principal: nombre, info, img y redes
-const principalC = document.getElementById('principal');
+document.getElementById('portfolioBtn').addEventListener('click', function() {
+  const mainSection = document.getElementById('mainSection');
+  const portfolioSection = document.getElementById('portfolioSection');
 
-//Contenido de portafolio
-const portfolio = document.getElementById('portfolio');
-
-//cuando el usuario toque show button, se muestra el contenido del portafolio y se oculta el principal
-const showButton = document.getElementById('portafolio');
-showButton.addEventListener('click', () => {
-  //se llama a la variable que consigue del documento y se le dice que el estilo a mostrar, será o bien block (mostrar) o none (ocultar)
-  portfolio.style.display = 'block';
-  principalC.style.display = 'none';
-  //en este caso, el portafolio es el contenido que se muestra, por lo que se oculta el principal
+  // Oculta mainSection y muestra portfolioSection
+  mainSection.style.display = 'none';
+  portfolioSection.style.display = 'block';
 });
 
-const closeButton = document.getElementById('close');
-closeButton.addEventListener('click', () => {
-  //se llama a la variable que consigue del documento y se le dice que el estilo a mostrar, será o bien block (mostrar) o none (ocultar)
-  portfolio.style.display = 'none';
-  principalC.style.display = 'block';
-  //en este caso, el principal es el contenido que se muestra, por lo que se oculta el portafolio
-});
+// Botón de cerrar para volver a mostrar el mainSection
+document.getElementById('close').addEventListener('click', function() {
+  const mainSection = document.getElementById('mainSection');
+  const portfolioSection = document.getElementById('portfolioSection');
 
-document.getElementById('portafolio').addEventListener('click', function() {
-  const container = document.querySelector('.container');
-  container.innerHTML = `
-      <ul>
-          ${Array.from({length: 20}, (_, i) => `<li class="lista">Elemento ${i + 1}</li>`).join('')}
-      </ul>
-  `;
+  // Muestra mainSection y oculta portfolioSection
+  mainSection.style.display = 'block';
+  portfolioSection.style.display = 'none';
 });
