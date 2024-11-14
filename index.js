b4b3c9d0690629cd28d1f0123ab2f71b7d7ab6ff
torofms37 <torofms37@gmail.com>
2024-11-14 09:16:27 -0700

    nuevas cosas a los colores

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-document.getElementById('portfolioBtn').addEventListener('click', function() {
-  const mainSection = document.getElementById('mainSection');
-  const portfolioSection = document.getElementById('portfolioSection');
-
-  // Oculta mainSection y muestra portfolioSection
-  mainSection.style.display = 'none';
-  portfolioSection.style.display = 'block';
-});
-
-// Botón de cerrar para volver a mostrar el mainSection
-document.getElementById('portfolioBtn').addEventListener('click', function() {
-  const mainSection = document.getElementById('mainSection');
-  const portfolioSection = document.getElementById('portfolioSection');
-
-  // Muestra mainSection y oculta portfolioSection
-  mainSection.style.display = 'block';
-  portfolioSection.style.display = 'none';
-});
+const mainSection = document.getElementById('mainSection');
+const portfolioSection = document.getElementById('portfolioSection');
+// Muestra mainSection y oculta portfolioSection
 
 document.getElementById('portfolioBtn').addEventListener('click', function() {
   if (this.innerHTML === 'PORTAFOLIO') {
     this.innerHTML = 'INICIO';
+    mainSection.style.display = 'none';
+    portfolioSection.style.display = 'block';
   } else {
     this.innerHTML = 'PORTAFOLIO';
+    mainSection.style.display = 'block';
+    portfolioSection.style.display = 'none';
   }
 });
