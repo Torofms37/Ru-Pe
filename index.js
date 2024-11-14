72e1e9260c6e27630f2b97283e0000abcab13102
torofms37 <torofms37@gmail.com>
2024-11-13 19:09:56 -0700

    se agregó más ajustes a los cards y se cambi+o los li por button ya con estilos

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ document.getElementById('portfolioBtn').addEventListener('click', function() {
 });
 
 // Botón de cerrar para volver a mostrar el mainSection
-document.getElementById('close').addEventListener('click', function() {
+document.getElementById('portfolioBtn').addEventListener('click', function() {
   const mainSection = document.getElementById('mainSection');
   const portfolioSection = document.getElementById('portfolioSection');
 
   // Muestra mainSection y oculta portfolioSection
   mainSection.style.display = 'block';
   portfolioSection.style.display = 'none';
+});
+
+document.getElementById('portfolioBtn').addEventListener('click', function() {
+  if (this.innerHTML === 'PORTAFOLIO') {
+    this.innerHTML = 'INICIO';
+  } else {
+    this.innerHTML = 'PORTAFOLIO';
+  }
 });
